c94b9effef3c8dffc6b6d1148e71efd4d3c2594a
utku-guclu <utku_guclu@hotmail.com>
2023-10-22 23:19:27 +0300

    pdf upload drag&drop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import { cn } from "@/lib/utils";
 import Navbar from "../components/Navbar";
 import Providers from "../components/Providers";
 
-import "react-loading-skeleton/dist/skeleton.css"
+import "react-loading-skeleton/dist/skeleton.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
             inter.className
           )}
         >
+          <Toaster />
           <Navbar />
           {children}
         </body>
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -35,7 +35,7 @@ const Dashboard = () => {
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
         <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
 
-        <UploadButton />
+        <UploadButton isSubscribed={true}/>
       </div>
 
       {/* display all user files */}
