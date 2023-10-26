e1e6d32d31b0b30fa1f5d40f8a6883ca5b5a2236
utku-guclu <utku_guclu@hotmail.com>
2023-10-26 22:44:09 +0300

    pinecone-openai

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,31 +2,83 @@ import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+
+import { getPineconeClient } from "@/lib/pinecone";
+
 const f = createUploadthing();
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
-    .middleware(async ({ req }) => {
-      const { getUser } = getKindeServerSession();
-      const user = getUser();
+    // Define as many FileRoutes as you like, each with a unique routeSlug
+    pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
+        .middleware(async ({ req }) => {
+            const { getUser } = getKindeServerSession();
+            const user = getUser();
 
-      if (!user || !user.id) throw new Error("Unauthorized");
+            if (!user || !user.id) throw new Error("Unauthorized");
 
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      const createdFile = await db.file.create({
-        data: {
-          key: file.key,
-          name: file.name,
-          userId: metadata.userId,
-          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-          uploadStatus: "PROCESSING",
-        },
-      });
-    }),
+            return { userId: user.id };
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            const createdFile = await db.file.create({
+                data: {
+                    key: file.key,
+                    name: file.name,
+                    userId: metadata.userId,
+                    url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+                    uploadStatus: "PROCESSING",
+                },
+            });
+
+            try {
+                const response = await fetch(
+                    `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+                );
+                const blob = await response.blob();
+
+                // langchain
+                const loader = new PDFLoader(blob);
+
+                const pageLevelDocs = await loader.load();
+
+                const pagesAmt = pageLevelDocs.length;
+
+                // vectorize and index entire document
+                const pinecone = await getPineconeClient();
+                const pineconeIndex = pinecone.Index("smartpdf");
+
+                const embeddings = new OpenAIEmbeddings({
+                    openAIApiKey: process.env.OPENAI_API_KEY,
+                });
+
+                await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+                    //@ts-ignore
+                    pineconeIndex,
+                    namespace: createdFile.id,
+                });
+
+                await db.file.update({
+                    data: {
+                        uploadStatus: "SUCCESS",
+                    },
+                    where: {
+                        id: createdFile.id,
+                    },
+                });
+            } catch (error) {
+                await db.file.update({
+                    data: {
+                        uploadStatus: "FAILED",
+                    },
+                    where: {
+                        id: createdFile.id,
+                    },
+                });
+            }
+        }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,4 +1,4 @@
-import ChatWrapper from "@/components/ChatWrapper";
+import ChatWrapper from "@/components/chat/ChatWrapper";
 import PdfRenderer from "@/components/PdfRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
@@ -34,12 +34,12 @@ const Page = async ({ params }: PageProps) => {
         {/* left side */}
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-            <PdfRenderer url={file.url}/>
+            <PdfRenderer url={file.url} />
           </div>
         </div>
         {/* right side */}
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:2-96 lg:border-l lg:border-0">
-          <ChatWrapper />
+          <ChatWrapper fileId={file.id} />
         </div>
       </div>
     </div>
